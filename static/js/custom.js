1edf8c01740a07759c84030990c8aa3589822482
Enrico Testori <hypertesto@gmail.com>
2020-01-20 22:29:31 +0100

    Removed non working stats endpoint

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,13 +1,1 @@
-    (function(f, a, t, h, o, m){
-        a[h]=a[h]||function(){
-                    (a[h].q=a[h].q||[]).push(arguments)
 
-        };
-            o=f.createElement('script'),
-                m=f.getElementsByTagName('script')[0];
-            o.async=1; o.src=t; o.id='fathom-script';
-            m.parentNode.insertBefore(o,m)
-
-    })(document, window, '//stats.hypertesto.me/tracker.js', 'fathom');
-fathom('set', 'siteId', 'XDFSG');
-fathom('trackPageview');
